7c1e8e27b11d28a7c5f7545bb3ce7d4169199944
Khoirul Anam <106360903+Khoirul0513@users.noreply.github.com>
2022-05-28 15:07:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,10 @@
-#KhoirulanamProjects#
 <?php
 #
 # Kode Awalan
 #    
 function cetakhello() {
   echo "Hello World";
-
+# 19112484  
 }
-$NIM = "19112484";
-echo "My car is " . $NIM . "<br>";
+
 ?>
